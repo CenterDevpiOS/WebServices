0e64e78c91e1d07ed4198bad3caffcda2f6157c2
CenterDevpiOS <marin.jose2011@gmail.com>
2015-05-15 22:21:33 -0430

    Create autopista.js

---
+++ API/autopista.js
@@ -0,0 +1,26 @@
+[
+  {
+    "Autopista": "Francisco Fajardo", 
+    "Estado": [
+      "Caracas"
+    ]
+  }, 
+  {
+    "Autopista": "Francisco de Miranda", 
+    "Estado": [
+      "Caracas"
+    ]
+  }
+  {
+    "Autopista": "Cota Mil", 
+    "Estado": [
+      "Caracas"
+    ]
+  }
+  {
+    "Autopista": "ARG", 
+    "Estado": [
+      "Valencia"
+    ]
+  }
+]
